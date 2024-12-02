8a3a530ddcd92815b117db67b1451be4ad6c677c
oppen321 <zj18139624826@gmail.com>
2024-12-02 19:24:09 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -10,9 +10,11 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 移除要替换的包
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,adguardhome}
+rm -rf feeds/packages/net/alist feeds/luci/applications/luci-app-alist
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/net/samba4
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
@@ -30,4 +32,76 @@ git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/l
 
 # Themes主题
 git clone --depth 1 https://github.com/sbwml/luci-theme-argon package/luci-theme-argon
+cp -f $GITHUB_WORKSPACE/images/bg.webp package/luci-theme-argon/luci-theme-argon/htdocs/luci-static/argon/img/bg.webp
+
+# OpenAppFilter
+git clone --depth 1 https://github.com/sbwml/OpenAppFilter package/OpenAppFilter
+
+# samba4
+git clone https://github.com/sbwml/feeds_packages_net_samba4 feeds/packages/net/samba4
+
+# SSRP & Passwall
+git clone https://github.com/sbwml/openwrt_helloworld package/helloworld -b v5
+
+# alist
+git clone https://github.com/sbwml/openwrt-alist package/new/alist
+
+# Mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/new/mosdns
+
+# frpc translation
+sed -i 's,发送,Transmission,g' feeds/luci/applications/luci-app-transmission/po/zh_Hans/transmission.po
+sed -i 's,frp 服务器,FRP 服务器,g' feeds/luci/applications/luci-app-frps/po/zh_Hans/frps.po
+sed -i 's,frp 客户端,FRP 客户端,g' feeds/luci/applications/luci-app-frpc/po/zh_Hans/frpc.po
+
+# SQM Translation
+mkdir -p feeds/packages/net/sqm-scripts/patches
+curl -s https://init2.cooluc.com//openwrt/patch/sqm/001-help-translation.patch > feeds/packages/net/sqm-scripts/patches/001-help-translation.patch
+
+# unzip
+rm -rf feeds/packages/utils/unzip
+git clone https://github.com/sbwml/feeds_packages_utils_unzip feeds/packages/utils/unzip
+
+# default config
+sed -i 's/#aio read size = 0/aio read size = 0/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#aio write size = 0/aio write size = 0/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/bind interfaces only = yes/bind interfaces only = no/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#create mask/create mask/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#directory mask/directory mask/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/0666/0644/g;s/0744/0755/g;s/0777/0755/g' feeds/luci/applications/luci-app-samba4/htdocs/luci-static/resources/view/samba4.js
+sed -i 's/0666/0644/g;s/0777/0755/g' feeds/packages/net/samba4/files/samba.config
+sed -i 's/0666/0644/g;s/0777/0755/g' feeds/packages/net/samba4/files/smb.conf.template
+
+# default settings
+git clone https://github.com/sbwml/default-settings package/default-settings -b openwrt-24.10
+
+# ddns - fix boot
+sed -i '/boot()/,+2d' feeds/packages/net/ddns-scripts/files/etc/init.d/ddns
+
+# luci-app-webdav
+git clone https://github.com/sbwml/luci-app-webdav package/new/luci-app-webdav
+
+# wwan
+git clone https://github.com/sbwml/wwan-packages package/wwan
+
+# luci-app-filemanager
+git clone https://github.com/sbwml/luci-app-filemanager package/luci-app-filemanager
+
+# node - prebuilt
+rm -rf feeds/packages/lang/node
+git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt feeds/packages/lang/node -b packages-24.10
+
+# aria2 & ariaNG
+rm -rf feeds/packages/net/ariang
+rm -rf feeds/luci/applications/luci-app-aria2
+git clone https://github.com/sbwml/ariang-nginx package/new/ariang-nginx
+rm -rf feeds/packages/net/aria2
+git clone https://github.com/sbwml/feeds_packages_net_aria2 -b 22.03 feeds/packages/net/aria2
+
+# netkit-ftp
+git clone https://github.com/sbwml/package_new_ftp package/ftp
+
+# nethogs
+git clone https://github.com/sbwml/package_new_nethogs package/nethogs
 
