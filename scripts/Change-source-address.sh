28249a1883a3a60185cdb2d67a10a96ea50a34f2
oppen321 <zj18139624826@gmail.com>
2024-12-03 11:17:22 +0800

    Update Change-source-address.sh

--- scripts/Change-source-address.sh
+++ scripts/Change-source-address.sh
@@ -6,7 +6,7 @@ mkdir -p files/etc/opkg
 NEW_SOURCES="
 src/gz openwrt_base https://mirrors.aliyun.com/openwrt/releases/24.10.0-rc1/packages/x86_64/base
 src/gz openwrt_luci https://mirrors.aliyun.com/openwrt/releases/24.10.0-rc1/packages/x86_64/luci
-src/gz openwrt_packages https://mirrors.aliyun.com/openwrt/releases/24.10.0-rc1/packages
+src/gz openwrt_packages https://mirrors.aliyun.com/openwrt/releases/24.10.0-rc1/packages/x86_64/packages
 src/gz openwrt_routing https://mirrors.aliyun.com/openwrt/releases/24.10.0-rc1/packages/x86_64/routing
 src/gz openwrt_telephony https://mirrors.aliyun.com/openwrt/releases/24.10.0-rc1/packages/x86_64/telephony
 "
