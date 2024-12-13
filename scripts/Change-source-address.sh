1d424285eb273322e905aafb3b900455049279fb
oppen321 <zj18139624826@gmail.com>
2024-12-14 01:04:47 +0800

    Update Change-source-address.sh

--- scripts/Change-source-address.sh
+++ scripts/Change-source-address.sh
@@ -4,11 +4,11 @@ mkdir -p files/etc/opkg
 
 # 设置新的源地址
 NEW_SOURCES="
-src/gz openwrt_base https://mirrors.aliyun.com/openwrt/releases/24.10.0-rc1/packages/x86_64/base
-src/gz openwrt_luci https://mirrors.aliyun.com/openwrt/releases/24.10.0-rc1/packages/x86_64/luci
-src/gz openwrt_packages https://mirrors.aliyun.com/openwrt/releases/24.10.0-rc1/packages/x86_64/packages
-src/gz openwrt_routing https://mirrors.aliyun.com/openwrt/releases/24.10.0-rc1/packages/x86_64/routing
-src/gz openwrt_telephony https://mirrors.aliyun.com/openwrt/releases/24.10.0-rc1/packages/x86_64/telephony
+src/gz openwrt_base https://mirrors.aliyun.com/openwrt/releases/24.10.0-rc2/packages/x86_64/base
+src/gz openwrt_luci https://mirrors.aliyun.com/openwrt/releases/24.10.0-rc2/packages/x86_64/luci
+src/gz openwrt_packages https://mirrors.aliyun.com/openwrt/releases/24.10.0-rc2/packages/x86_64/packages
+src/gz openwrt_routing https://mirrors.aliyun.com/openwrt/releases/24.10.0-rc2/packages/x86_64/routing
+src/gz openwrt_telephony https://mirrors.aliyun.com/openwrt/releases/24.10.0-rc2/packages/x86_64/telephony
 "
 
 # 创建 opkg 配置文件并写入源地址
